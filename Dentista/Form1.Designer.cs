d388a2b71044301a1939ed6cf91ace14f4ca3cc1
Genaro Yair Enciso Luna <genaroyair@hotmail.com>
2020-03-16 15:52:27 -0600

    primera imagen

---
+++ Dentista/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Dentista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(172, 39);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(212, 20);
+            this.textBoxId.TabIndex = 1;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(172, 79);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(212, 20);
+            this.textBoxNombre.TabIndex = 3;
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Location = new System.Drawing.Point(172, 123);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(212, 20);
+            this.textBoxDireccion.TabIndex = 4;
+            // 
+            // textBoxEdad
+            // 
+            this.textBoxEdad.Location = new System.Drawing.Point(172, 171);
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(212, 20);
+            this.textBoxEdad.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dirección";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Edad";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 25);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(222, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 25);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Actualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(57, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 25);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Eliminar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.imageBox.Location = new System.Drawing.Point(406, 23);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(424, 152);
+            this.imageBox.TabIndex = 13;
+            this.imageBox.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(437, 205);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 25);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Buscar en archivos";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(577, 205);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 25);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Subir imagen";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(718, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 36);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Buscar imagen en la nube";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 276);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Dentista/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+namespace Dentista
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseClient client;
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "xL72WVNg3zNeNfkWo2aQ7WRX4Nxhjqe1mbtCNru7",
+            BasePath = "https://dentista-99f4c.firebaseio.com/"
+        }; 
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if(client != null)
+            {
+                //MessageBox.Show("Conexion establecida");
+            }
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            var data = new Data {
+                Id = textBoxId.Text,
+                Name = textBoxEdad.Text,
+                Address = textBoxDireccion.Text,
+                Age = textBoxNombre.Text
+                };
+
+            SetResponse response = await client.SetTaskAsync("Cliente/" + textBoxId.Text,data);
+            Data result = response.ResultAs<Data>();
+
+            MessageBox.Show("Insertado correctamente " + result.Name);
+
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.GetTaskAsync("Cliente/" + textBoxId.Text);
+
+            try
+            {
+                Data obj = response.ResultAs<Data>();
+
+                if (obj != null)
+                {
+                    textBoxId.Text = obj.Id;
+                    textBoxEdad.Text = obj.Name;
+                    textBoxDireccion.Text = obj.Address;
+                    textBoxNombre.Text = obj.Age;
+
+                    MessageBox.Show("Registro encontrado");
+                }
+
+
+            }
+            catch (Exception w)
+            {
+                Console.WriteLine(w);
+                MessageBox.Show("Registro no encontrado verifique su busqueda");
+
+            }
+
+            
+
+        }
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = textBoxId.Text,
+                Name = textBoxEdad.Text,
+                Address = textBoxDireccion.Text,
+                Age = textBoxNombre.Text
+            };
+
+            FirebaseResponse response = await client.UpdateTaskAsync("Cliente/" + textBoxId.Text, data);
+            Data result = response.ResultAs<Data>();
+            MessageBox.Show("Registro modificado exitosamente");
+
+        }
+
+        private async void Button4_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.DeleteTaskAsync("Cliente/"+textBoxId.Text);
+
+            MessageBox.Show("Registro eliminado correctamente");    
+        }
+
+        private async void Button6_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageBox.Image.Save(ms, ImageFormat.Jpeg);
+
+            byte [] a = ms.GetBuffer();
+
+            string output = Convert.ToBase64String(a);
+
+            var data = new ImageModal
+            {
+                Img = output
+            };
+
+            SetResponse response = await client.SetTaskAsync("Imagen/" + textBoxId.Text,data);
+            ImageModal result = response.ResultAs<ImageModal>();
+
+            MessageBox.Show("Se inserto la imagen correctamente");
+
+            imageBox.Image = null;
+
+        }
+
+        private async void Button7_Click(object sender, EventArgs e)
+        {
+            try {
+
+                FirebaseResponse response = await client.GetTaskAsync("Imagen/" + textBoxId.Text);
+                ImageModal image = response.ResultAs<ImageModal>();
+                                
+                byte[] b = Convert.FromBase64String(image.Img);
+
+                MemoryStream ms = new MemoryStream();
+                ms.Write(b, 0, Convert.ToInt32(b.Length));
+
+                Bitmap bm = new Bitmap(ms, false);
+                ms.Dispose();
+
+                imageBox.Image = bm;
+            }
+            catch (Exception w)
+            {
+                Console.WriteLine(w);
+                MessageBox.Show("No se encontro ninguna imagen");
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Selecciona la imagen";
+            ofd.Filter = "Image files(*.jpg) | *.jpg";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image img = new Bitmap(ofd.FileName);
+                imageBox.Image = img.GetThumbnailImage(424, 152, null, new IntPtr());
+            }
+        }
+    }
+}
